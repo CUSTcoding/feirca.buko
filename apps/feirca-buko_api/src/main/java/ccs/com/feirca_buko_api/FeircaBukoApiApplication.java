8c2e2074c8eb462c7b8ed97b1011cc31bc9024e0
CustCoding <custodiotitossetitosse@gmail.com>
2026-04-07 00:07:28 +0200

    chore(deps): update project dependencies

---
+++ apps/feirca-buko_api/src/main/java/ccs/com/feirca_buko_api/FeircaBukoApiApplication.java
@@ -0,0 +1,13 @@
+package ccs.com.feirca_buko_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeircaBukoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeircaBukoApiApplication.class, args);
+	}
+
+}
---
+++ apps/feirca-buko_api/src/test/java/ccs/com/feirca_buko_api/FeircaBukoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ccs.com.feirca_buko_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeircaBukoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
