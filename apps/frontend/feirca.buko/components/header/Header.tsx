91cfa7de726848186aa1c4dc58a09b6187add10e
CustCoding <custodiotitossetitosse@gmail.com>
2026-05-14 16:56:34 +0200

    feat(header): add header components

---
+++ apps/frontend/feirca.buko/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import { SearchIcon, Bell } from "lucide-react"
+
+
+export function Header() {
+  return (
+    <header className="flex h-16 shrink-0 items-center  justify-end gap-2">
+        
+        
+
+    </header>   
+     
+  )
+}
